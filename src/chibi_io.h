ae50113fd53eb8fef7a96f39bf4388e249d7bc07
Lee Witek <neomech@gmail.com>
2016-09-20 22:19:05 +0100

    corrected scan codes

--- src/chibi_io.h
+++ src/chibi_io.h
@@ -3,30 +3,37 @@
 
 #include <Arduino.h>
 
+/*
+ Keylayout:
+    0 1 2 3   Ext0
+    4 5 6 7	  Ext1
+    8 9 A B	  Ext2
+    C D E F   Ext3
+*/
+
 namespace Chibi {
 
-	const uint8_t KEY_NONE = 0;
-	const uint8_t KEY_0 = 1;
-	const uint8_t KEY_1 = 2;
-	const uint8_t KEY_2 = 3;
-	const uint8_t KEY_3 = 4;
-	const uint8_t KEY_4 = 5;
-	const uint8_t KEY_5 = 6;
-	const uint8_t KEY_6 = 7;
-	const uint8_t KEY_7 = 8;
+	const uint8_t KEY_0 = 19;
+	const uint8_t KEY_1 = 18;
+	const uint8_t KEY_2 = 17;
+	const uint8_t KEY_3 = 16;
+	const uint8_t KEY_4 = 14;
+	const uint8_t KEY_5 = 15;
+	const uint8_t KEY_6 = 16;
+	const uint8_t KEY_7 = 17;
 	const uint8_t KEY_8 = 9;
-	const uint8_t KEY_9 = 10;
-	const uint8_t KEY_A = 11;
-	const uint8_t KEY_B = 12;
-	const uint8_t KEY_C = 13;
-	const uint8_t KEY_D = 14;
-	const uint8_t KEY_E = 15;
-	const uint8_t KEY_F = 16;
+	const uint8_t KEY_9 = 8;
+	const uint8_t KEY_A = 7;
+	const uint8_t KEY_B = 6;
+	const uint8_t KEY_C = 4;
+	const uint8_t KEY_D = 3;
+	const uint8_t KEY_E = 2;
+	const uint8_t KEY_F = 1;
 
-	const uint8_t KEY_EX1 = 17;
-	const uint8_t KEY_EX2 = 18;
-	const uint8_t KEY_EX3 = 19;
-	const uint8_t KEY_EX4 = 20;
+	const uint8_t KEY_EX0 = 15;
+	const uint8_t KEY_EX1 = 10;
+	const uint8_t KEY_EX2 = 5;
+	const uint8_t KEY_EX3 = 0;
 
 	struct KeyReceiver {
 		virtual void onKey(uint8_t scancode, bool pressed) = 0;
