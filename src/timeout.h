590ca53819ef27416f50663071d8c846df115c1d
Lee Witek <neomech@gmail.com>
2016-09-22 10:50:06 +0100

    implemented timeouts

---
+++ src/timeout.h
@@ -0,0 +1,36 @@
+#ifndef TIMEOUT_H
+#define TIMEOUT_H
+
+#include <Arduino.h>
+
+class TimeOutus {
+	uint32_t m_start;
+	uint32_t m_period;
+
+  public:
+	TimeOutus(uint32_t timeoutus = 0) {
+		m_start = (uint32_t)micros();
+		m_period = timeoutus;
+	}
+
+	bool hasTimedOut() const {
+		return (micros() - m_start) < m_period;
+	}
+};
+
+class TimeOutms {
+	uint32_t m_start;
+	uint32_t m_period;
+
+  public:
+	TimeOutms(uint32_t timeoutms = 0) {
+		m_start = (uint32_t)millis();
+		m_period = timeoutms;
+	}
+
+	bool hasTimedOut() const {
+		return (millis() - m_start) < m_period;
+	}
+};
+
+#endif /* TIMEOUT_H */
