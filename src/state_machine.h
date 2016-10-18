197a08367e0c69152490793e30b39fed9d1e9ae3
Lee Witek <neomech@gmail.com>
2016-10-18 20:20:40 +0100

    dont run enter and leave phases until current update has completed.

--- src/state_machine.h
+++ src/state_machine.h
@@ -6,18 +6,31 @@ struct StateMachine {
   public:
 	enum Phase_t { None, Enter, Update, Leave };
 
-	StateMachine(objType* objPtr) : m_currentState(0), m_objPtr(objPtr) {
+	StateMachine(objType* objPtr)
+	    : m_currentState(0), m_gotoState(0), m_objPtr(objPtr), m_inUpdate(false) {
 	}
 
 	typedef void (objType::*stateFunction_t)(Phase_t phase);
 
 	void stateUpdate() {
 		if (m_currentState) {
+			m_inUpdate = true;
 			(m_objPtr->*m_currentState)(Update);
+			m_inUpdate = false;
+
+			if (m_gotoState) {
+				stateGoto(m_gotoState);
+				m_gotoState = 0;
+			}
 		}
 	}
 
 	void stateGoto(stateFunction_t state) {
+		if (m_inUpdate) {
+			m_gotoState = state;
+			return;
+		}
+
 		if (m_currentState) {
 			(m_objPtr->*m_currentState)(Leave);
 		}
@@ -31,7 +44,9 @@ struct StateMachine {
 
   private:
 	stateFunction_t m_currentState;
+	stateFunction_t m_gotoState;
 	objType* m_objPtr;
+	bool m_inUpdate;
 };
 
 #endif /* STATE_MACHINE_H */
